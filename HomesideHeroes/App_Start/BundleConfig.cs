d4306bf4577b690b66cffc3373aafa9b0e693753
Danny Brown <csengineer13@gmail.com>
2016-11-06 20:59:12 -0500

    Cooler styles

--- HomesideHeroes/App_Start/BundleConfig.cs
+++ HomesideHeroes/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace HomesideHeroes
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/cyborg.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
